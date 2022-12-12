07b28d2f5b6fd48aea396cd9858292ef6546a970
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-12-11 22:23:31 -0500

    added an html file to allow the program to go onto a website. uses JS and the jdoodle api. max 200 runs.

--- src/InfixToPostFixWeb.java
+++ src/InfixToPostFixWeb.java
@@ -2,7 +2,66 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public interface Stack<T> {
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String expression = in.nextLine();
+        System.out.println("Before: " + expression);
+        System.out.println("After: " + convertToPostFix(expression));
+
+
+    }
+
+    public static int priority (char character){
+        return switch (character) {
+            case '+', '-' -> 1;
+            case '*', '/' -> 2; //second-highest priority
+            case '^' -> 3;
+            default -> //highest priority
+                    -1;
+        };
+    }
+
+    public static String convertToPostFix(String infix){
+        FixedStack<Character> operatorStack = new FixedStack<>(Character.class, 20); // init stack of capacity 10
+        StringBuilder postfix = new StringBuilder(); //empty string
+        for(int i = 0; i < infix.length(); ++i){ //while i < infix length, increment i
+            char currentChar = infix.charAt(i); //the character at the current iteration of the for loop
+            if(Character.isLetterOrDigit(currentChar)){
+                postfix.append(currentChar); //add currentChar to postfix
+            }
+            else if(currentChar == '('){ //if current char is (
+                operatorStack.push(currentChar); //push currentChar from operatorStack
+            }
+            else if(currentChar == ')'){ //if current char is ')'
+                while(!operatorStack.isEmpty() && operatorStack.peek() != '(') {// while operator stack is not empty and peek is not (
+                    postfix.append(operatorStack.peek()); //add operatorStack.peek to postfix
+                    operatorStack.pop();
+                }
+                operatorStack.pop();
+            }
+            else {// when an operator is encountered
+                //while operator stack is not empty and priority at currentChar <= priority at peek
+                while (!operatorStack.isEmpty() && priority(currentChar) <= priority(operatorStack.peek())){
+                    postfix.append(operatorStack.peek());
+                    operatorStack.pop(); //pop off last element
+                }
+                operatorStack.push(currentChar);
+            }
+        }
+        //pop out all the operators from stack
+        while(!operatorStack.isEmpty()) { //while operator stack is not empty
+            if (operatorStack.peek() == '('){
+                return "Invalid Expression.";
+            }
+            postfix.append(operatorStack.peek()); //add peek to postfix
+            operatorStack.pop();
+        }
+
+        return postfix.toString();
+    }
+
+    public static interface Stack<T> {
     void push (T newEntry); //add new entry to the top of the stack. // throw exception if empty
     T pop(); //removes and returns the stack's top entry. //throw exception if empty
     T peek(); //retrieve stack's top entry without changing the stack. throw exception if empty.
@@ -10,9 +69,9 @@ public interface Stack<T> {
     void clear(); //remove all entries from the stack.
 
     void show(); //show all elements in the stack.
-}
+    }
 
-public class FixedStack<T> implements Stack<T>{
+    public static class FixedStack<T> implements Stack<T>{
     private final T[] stack;
     private int top = 0;
 
@@ -70,62 +129,12 @@ public class FixedStack<T> implements Stack<T>{
     }
 }
 
-public class InfixToPostFixWeb {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String expression = in.next();
-
-    }
-
-    public static int priority (char character){
-        return switch (character) {
-            case '+', '-' -> 1;
-            case '*', '/' -> 2; //second-highest priority
-            case '^' -> 3;
-            default -> //highest priority
-                    -1;
-        };
-    }
-
-    public static String convertToPostFix(String infix){
-        FixedStack<Character> operatorStack = new FixedStack<>(Character.class, 20); // init stack of capacity 10
-        StringBuilder postfix = new StringBuilder(); //empty string
-        for(int i = 0; i < infix.length(); ++i){ //while i < infix length, increment i
-            char currentChar = infix.charAt(i); //the character at the current iteration of the for loop
-            if(Character.isLetterOrDigit(currentChar)){
-                postfix.append(currentChar); //add currentChar to postfix
-            }
-            else if(currentChar == '('){ //if current char is (
-                operatorStack.push(currentChar); //push currentChar from operatorStack
-            }
-            else if(currentChar == ')'){ //if current char is ')'
-                while(!operatorStack.isEmpty() && operatorStack.peek() != '(') {// while operator stack is not empty and peek is not (
-                    postfix.append(operatorStack.peek()); //add operatorStack.peek to postfix
-                    operatorStack.pop();
-                }
-                operatorStack.pop();
-            }
-            else {// when an operator is encountered
-                //while operator stack is not empty and priority at currentChar <= priority at peek
-                while (!operatorStack.isEmpty() && priority(currentChar) <= priority(operatorStack.peek())){
-                    postfix.append(operatorStack.peek());
-                    operatorStack.pop(); //pop off last element
-                }
-                operatorStack.push(currentChar);
-            }
-        }
-        //pop out all the operators from stack
-        while(!operatorStack.isEmpty()) { //while operator stack is not empty
-            if (operatorStack.peek() == '('){
-                return "Invalid Expression.";
-            }
-            postfix.append(operatorStack.peek()); //add peek to postfix
-            operatorStack.pop();
-        }
-
-        return postfix.toString();
-    }
-
 
 }
+
+
+
+
+
+
 
